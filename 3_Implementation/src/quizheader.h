949a09d64183077bec080c760720feb749dcae8b
259159 <80697213+259159@users.noreply.github.com>
2021-04-15 15:55:18 +0530

    Add files via upload

---
+++ 3_Implementation/src/quizheader.h
@@ -0,0 +1,33 @@
+/*
+@file   quizheader.h
+@author Suraksha S S
+@brief  Header file for quiz game
+@version 0.1
+@date 2021-04-14
+
+@copyright Copyright (c) 2021
+
+*/
+
+#ifndef __QUIZHEADER_H__
+#define __QUIZHEADER_H__
+
+
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+/*
+@brief  function to start the quiz
+
+*/
+void startquiz();
+
+/*
+@brief  function to read the instructions to start the quiz
+
+*/
+void help();
+
+#endif
+
